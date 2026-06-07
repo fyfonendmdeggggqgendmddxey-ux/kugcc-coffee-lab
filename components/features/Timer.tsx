ee9127c795413efaad0ea03ca0574722c50de2b1
kugcc <kugcc@example.com>
2026-06-08 06:15:36 +0900

    Fix JSX syntax error to allow Vercel build

--- components/features/Timer.tsx
+++ components/features/Timer.tsx
@@ -235,6 +235,7 @@ export default function Timer({
                               </option>
                           ))}
                       </optgroup>
+                  )}
                   </select>
               </div>
         </div>
